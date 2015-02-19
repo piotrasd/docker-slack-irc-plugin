65628be9b79fac11e65d1bc03d48ace89f39266e
Jonas Friedmann <j@frd.mn>
2015-02-19 10:53:21 +0100

    Add example configuration

---
+++ opt/config-example.js
@@ -0,0 +1,25 @@
+var slackbot = require('./lib/bot');
+
+var config = {
+    // required
+    server: 'irc.freenode.com',
+    port: 6667,
+    secure: false,
+    password: '(optional)',
+    nick: 'slackbot',
+    username: 'slackbot-username',
+    token: 'XXXX-XXXXXXXXXX-XXXXXXXXXX-XXXXXXXXXX-XXXXXX',
+    channels: {
+        '#irc-channel password(optional)': '#slack-channel'
+    },
+    users: {
+        '~irclogin': 'slackuser'
+    },
+    // optionals
+    silent: false, // default
+    // node-irc options
+    floodProtection: true
+};
+
+var slackbot = new slackbot.Bot(config);
+slackbot.listen();
